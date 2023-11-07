89dff9f98991bcefbb56c1df2ec7a569415b6f49
wagenhoffer <77755517+wagenhoffer@users.noreply.github.com>
2023-11-07 17:08:14 -0500

    self_prop changes but still unstable

--- scripts/self_propelled.jl
+++ scripts/self_propelled.jl
@@ -1,5 +1,5 @@
 using Puffer
-
+using LinearAlgebra
 using Plots
 
 """
@@ -20,20 +20,26 @@ end
 
 begin
     #scripting
+    (f, k, mass) = (4.0f0, 0.25f0, 0.1f0)
     upm = deepcopy(defaultDict)
     upm[:Nt] = 64
     upm[:N] = 64
-    upm[:Ncycles] = 10
+    upm[:Ncycles] = 4
     upm[:Uinf] = 1.0
     upm[:kine] = :make_ang
     upm[:pivot] = 0.0
     upm[:foil_type] = :make_naca
     upm[:thick] = 0.12
-    upm[:f] = 1.0
-    upm[:k] = 1.25
+    upm[:motion_parameters] = [0.1]
+
+    upm[:f] = f
+    upm[:k] = k
+    mass = mass
+    U_init= 0.2858841768338524
+
 
     foil, flow = init_params(; upm...)
-    mass = 1.0
+    
     turns = zeros(flow.Ncycles * flow.N)
     wake = Wake(foil)
     Us = zeros(2, flow.Ncycles * flow.N)
@@ -43,10 +49,8 @@ begin
     phi = zeros(foil.N)
     coeffs = zeros(4, flow.Ncycles * flow.N)
     ps = zeros(foil.N, flow.Ncycles * flow.N)
-    stop = π
-    steps = flow.N * flow.Ncycles
-    turns = collect((foil.θ):(stop / steps):stop) .* 0
-    # trs = 0.0
+
+
     anim = Animation()
     for i in 1:(flow.Ncycles * flow.N)
         if flow.n != 1
@@ -56,7 +60,7 @@ begin
         if i == 1
             U = _propel(foil, flow; U = [0.0, 0.0], mass = mass, turnto = turns[i])
             # Cast this to  2d vel
-            U = [0.95, 0.0]
+            U = [U_init, 0.0]
         else
             U = _propel(foil,
                 flow;
@@ -103,28 +107,22 @@ begin
 
     @show maximum(coeffs[3,64:end])
     @show maximum(Us[1,64:end])
-    # gif(anim, "./images/self_prop_ang.gif", fps = 10)
-    plot(Us[1, 64:end])
-end
-    # MAKE A Plot FOR THE articial thrust signal
-    # t = (flow.Δt):(flow.Δt):(flow.N * flow.Δt * flow.Ncycles)
-    # plot(t, -0.2 .* cos.(4π .* foil.f .* t))
-function get_peaks(signal, period, num_periods)
-    " given a sinusoidal signal, skip the first period and then find the peaks"
-    signal = signal[period:end]
-    peaks = zeros(num_periods - 1)
-    for i in 1:(num_periods - 1)
-        start_index = (i - 1) * period + 1
-        end_index = i * period
-        peaks[i] = maximum(signal[start_index:end_index])
-    end
-    return peaks
+    gif(anim, "./images/self_prop_ang.gif", fps = 30)
+    # plot(Us[1, 64:end])
 end
 
-peaks = get_peaks(coeffs[3,:], flow.N, flow.Ncycles)
+function get_norms(signal, T, num_periods; norm_index = 2)
+    "given a sinusoidal signal, skip the first period and then return the L2 norm"
+    chopped  = [signal[i*T + 1:(i+1)*T] for i in 1:(num_periods-1)]
+    norm.(chopped, norm_index)
+end
+
+
+tolerance = 1e-3    
+peaks = get_norms(coeffs[3,:], flow.N, flow.Ncycles)
 diff(peaks)[end] < tolerance
 
-function find_Us(;U_init = 1, St= 0.3, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
+function find_Us(;U_init = 1.0, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
     upm = deepcopy(defaultDict)
     upm[:Nt] = 64
     upm[:N] = 64
@@ -136,7 +134,7 @@ function find_Us(;U_init = 1, St= 0.3, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
     upm[:thick] = 0.12
     upm[:f] = f
     upm[:k] = k
-    a0 = St / upm[:f]
+    a0 = 0.1
     upm[:motion_parameters] = [a0]
 
     foil, flow = init_params(; upm...)
@@ -200,20 +198,23 @@ function find_Us(;U_init = 1, St= 0.3, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
     Us
 end
 
-function us_relax(;n_loops=25,tolerance=1e-4, Ncycles=4, f=1.0, k=1.0, mass = 1.0, St = 0.3)
-    U_init = 1.0        
-    for i in 1:n_loops
-        
-        Us = find_Us(U_init = U_init, Ncycles = Ncycles,mass=mass,f=f,k=k, St=St)
-        peaks = get_peaks(Us[1,:], flow.N, Ncycles)
+function us_relax(;n_loops=25, tolerance=1e-3, Ncycles=4, f=1.0, k=1.0, mass = 1.0)
+    U_init = 1.0
+    for i in 1:n_loops     
+        # (f, k, mass) = (0.25f0, 1.125f0, 0.1f0)           
+        Us = find_Us(;U_init = U_init, Ncycles = Ncycles, mass=mass, f=f, k=k)
+        # peaks = get_peaks(Us[1,:], flow.N, Ncycles)
+        peaks = get_norms(Us[1,:], flow.N, Ncycles; norm_index=2)
         convergence = diff(peaks)[end]
         if abs(convergence) < tolerance
             return U_init
         end
-        U_init = convergence < 0 ?
+        U_init = convergence < 0 ?        
          minimum(Us[1,flow.N*Ncycles-1:end]) : 
-         maximum(Us[1,flow.N*Ncycles-1:end])
-        @show (f,k,mass), i, U_init, convergence
+         maximum(Us[1,flow.N*Ncycles-1:end])        
+        # U_init = shanks(get_norms(Us[1,:], flow.N, Ncycles; norm_index=Inf))
+        print("\t $i U_init = $U_init} Conv= $convergence \n")
+    
     end
     # return a negative value for non-converged cases
     return -U_init
@@ -222,55 +223,37 @@ end
 begin
     T = Float32
     N = 3
-    fs =LinRange{T}(0.4, 4, N)
-    ks = LinRange{T}(0.35, 2.0, N)
-    Sts = LinRange{T}(0.0125, 0.4, N)
-    masses = [LinRange{T}(0.1, 2.0, N)]
-    Uinits = zeros(length(fs),length(ks),length(masses))
-    for (i,f) in enumerate(fs)
-        for (j,k) in enumerate(ks)
-            for (l,mass) in enumerate(masses)
+    fs = LinRange{T}(0.5, 4, N)
+    ks = LinRange{T}(0.25, 2.0, N)
+    masses = [2.0, 1.0, 0.5] .|>T
+    Uinits = zeros(N,N,N)
+    for (l,mass) in enumerate(masses)
+        for (j,k) in enumerate(ks)        
+            for (i,f) in enumerate(fs)
+                @show (f,k,mass)
                 Uinits[i,j,l] = us_relax(;f=f,k=k,mass=mass)
             end
         end
     end
 end
 
-#count of Converged
-length(Uinits) - sum(Uinits .< 0)
-
-"""
-    trapezoid(y, a, b, n)
-
-Apply the trapezoid integration formula for integrand `f` over
-interval [`a`,`b`], broken up into `n` equal pieces. Returns
-the estimate, a vector of nodes, and a vector of integrand values at the
-nodes.
-"""
-function trapezoid(y, a, b, n)
-    h = (b - a) / n
-    t = range(a, b, length = n + 1)
-    # y = f.(t)
-    T = h * (sum(y[2:n]) + 0.5 * (y[1] + y[n + 1]))
-    return T, t, y
-end
-
 
 begin
     #scripting
+    (f, k, mass) = (0.5f0, 2.0f0, 2.0f0)
+    U_init = π
     upm = deepcopy(defaultDict)
     upm[:Nt] = 64
     upm[:N] = 64
-    upm[:Ncycles] = 2
+    upm[:Ncycles] = 4
     upm[:Uinf] = 1.0
-    upm[:kine] = :make_heave_pitch
+    upm[:kine] = :make_ang
     upm[:pivot] = 0.0
     upm[:foil_type] = :make_naca
     upm[:thick] = 0.12
-    upm[:f] = 1.0
-    h0 = 0.0
-    θ0 = deg2rad(4)
-    upm[:motion_parameters] = [h0, θ0]
+    upm[:f] = f
+    upm[:k] = k
+    upm[:motion_parameters] = [0.1]
 
     foil, flow = init_params(; upm...)
     # foil.θ= π/2*0.0
@@ -283,9 +266,7 @@ begin
     phi = zeros(foil.N)
     coeffs = zeros(4, flow.Ncycles * flow.N)
     ps = zeros(foil.N, flow.Ncycles * flow.N)
-    turnrad = -π * flow.Δt / 4.0
-    trs = zeros(flow.N * flow.Ncycles)
-    trs[(flow.N):(2 * flow.N - 32)] .= turnrad
+
     anim = Animation()
     # f = plot()
     for i in 1:(flow.Ncycles * flow.N)
@@ -296,25 +277,26 @@ begin
         if i == 1
             U = (foil)(flow)
             # Cast this to  2d vel
-            U = [0.0, 0.0]
+            U = [U_init, 0.0]
         else
             U = _propel(foil,
                 flow;
                 forces = coeffs[:, i - 1],
-                U = [0.0, 0.0],
-                mass = 1e-5,
-                turn = trs[i],)
+                U = U,
+                mass = mass, 
+                self_prop = true)
             # U = (foil)(flow)
         end
-        Us[:, i] = U
+        Us[:, i] .= U
+        U_bx = U[1]
         A, rhs, edge_body = make_infs(foil)
-        A[getindex.(A .== diag(A))] .= 0.5
-        setσ!(foil, flow)
-        turn_σ!(foil, flow, trs[i])
+
+        setσ!(foil, flow; U_b = U_bx)
+        get_panel_vels!(foil, flow)
+
         foil.wake_ind_vel = vortex_to_target(wake.xy, foil.col, wake.Γ, flow)
         normal_wake_ind = sum(foil.wake_ind_vel .* foil.normals, dims = 1)'
         foil.σs -= normal_wake_ind[:]
-
         buff = edge_body * foil.μ_edge[1]
         foil.μs = A \ (-rhs * foil.σs - buff)[:]
         set_edge_strength!(foil)
@@ -322,11 +304,12 @@ begin
         body_to_wake!(wake, foil, flow)
         wake_self_vel!(wake, flow)
         phi = get_phi(foil, wake)
-        p = panel_pressure(foil, flow, old_mus, old_phis, phi)
+        p = panel_pressure(foil, flow, old_mus, old_phis, phi; U_b = U_bx)
 
         old_mus = [foil.μs'; old_mus[1:2, :]]
         old_phis = [phi'; old_phis[1:2, :]]
         coeffs[:, i] = get_performance(foil, flow, p)
+
         ps[:, i] = p
         f = plot_current(foil, wake)
         plot!(f, ylims = (-1, 1))
@@ -335,7 +318,18 @@ begin
         # plot!(f, foil.foil[1,:],foil.foil[2,:], label="", aspect_ratio=:equal)
     end
     # f
-    gif(anim, "./images/handp.gif", fps = 10)
+    gif(anim, "./images/handp.gif", fps = 30)
+end
+function shanks(as)
+    "Shanks transformation for accelerating convergence of a sequence"
+    N = length(as)
+    if N < 3
+        return as
+    end
+    a = as[1:end - 2]
+    b = as[2:end - 1]
+    c = as[3:end]
+    return c .- ((c .- b).^2) ./ (a .- 2b .+ c)
 end
 
 begin
