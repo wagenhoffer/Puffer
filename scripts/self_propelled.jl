25449dd29da635cbfac750342688b8d8c9bbd473
wagenhoffer <77755517+wagenhoffer@users.noreply.github.com>
2023-11-07 09:23:54 -0500

    added back in Fence

--- scripts/self_propelled.jl
+++ scripts/self_propelled.jl
@@ -5,7 +5,7 @@ using Plots
 """
     turn_σ!(foil::Foil, flow::FlowParams, turn)
 
-This function modifies σ for a discrete turn, ie not part of the kinematics functional signal.
+This function modifies σ for a discrete turn, ie not part of the kinematics functional signal. 
 """
 function turn_σ!(foil::Foil, flow::FlowParams, turn)
     ff = get_mdpts(([foil._foil[1, :] .- foil.pivot foil._foil[2, :] .+
@@ -13,7 +13,7 @@ function turn_σ!(foil::Foil, flow::FlowParams, turn)
         foil.f,
         foil.k,
         flow.n * flow.Δt)] * rotation(-turn))') .+ foil.LE
-    vel = (foil.col - ff) ./ flow.Δt
+    vel = (foil.col - ff) ./ flow.Δt 
     foil.σs += vel[1, :] .* foil.normals[1, :] + vel[2, :] .* foil.normals[2, :]
     nothing
 end
@@ -106,7 +106,6 @@ begin
     # gif(anim, "./images/self_prop_ang.gif", fps = 10)
     plot(Us[1, 64:end])
 end
-
     # MAKE A Plot FOR THE articial thrust signal
     # t = (flow.Δt):(flow.Δt):(flow.N * flow.Δt * flow.Ncycles)
     # plot(t, -0.2 .* cos.(4π .* foil.f .* t))
@@ -171,7 +170,7 @@ function find_Us(;U_init = 1, St= 0.3, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
                 mass = mass,
                 self_prop = true,)        
         end
-       
+        
         Us[:, i] .= U
         U_bx = U[1]
         A, rhs, edge_body = make_infs(foil)
@@ -201,10 +200,10 @@ function find_Us(;U_init = 1, St= 0.3, f=1.0, k=1.0, mass = 1.0, Ncycles = 3)
     Us
 end
 
-function us_relax(;n_loops=25,tolerance=1e-4, Ncycles=4, f=1.0, k=1.0, mass = 1.0, St = 0.3,U_init = 1.0  )
-        
+function us_relax(;n_loops=25,tolerance=1e-4, Ncycles=4, f=1.0, k=1.0, mass = 1.0, St = 0.3)
+    U_init = 1.0        
     for i in 1:n_loops
-       
+        
         Us = find_Us(U_init = U_init, Ncycles = Ncycles,mass=mass,f=f,k=k, St=St)
         peaks = get_peaks(Us[1,:], flow.N, Ncycles)
         convergence = diff(peaks)[end]
@@ -212,9 +211,9 @@ function us_relax(;n_loops=25,tolerance=1e-4, Ncycles=4, f=1.0, k=1.0, mass = 1.
             return U_init
         end
         U_init = convergence < 0 ?
-         minimum(Us[1,flow.N*Ncycles-1:end]) :
+         minimum(Us[1,flow.N*Ncycles-1:end]) : 
          maximum(Us[1,flow.N*Ncycles-1:end])
-        @show (f,k,St, mass), i, U_init, convergence
+        @show (f,k,mass), i, U_init, convergence
     end
     # return a negative value for non-converged cases
     return -U_init
@@ -226,15 +225,12 @@ begin
     fs =LinRange{T}(0.4, 4, N)
     ks = LinRange{T}(0.35, 2.0, N)
     Sts = LinRange{T}(0.0125, 0.4, N)
-    masses = [0.5,1.0, 2.0].|> T
-    Uinits = zeros(N,N,N,N)
+    masses = [LinRange{T}(0.1, 2.0, N)]
+    Uinits = zeros(length(fs),length(ks),length(masses))
     for (i,f) in enumerate(fs)
         for (j,k) in enumerate(ks)
             for (l,mass) in enumerate(masses)
-                for (n,St) in enumerate(Sts)
-                    Uinits[i,j,l,n] = us_relax(;f=f,k=k,mass=mass, St=St, U_init=mass)
-                end
-                
+                Uinits[i,j,l] = us_relax(;f=f,k=k,mass=mass)
             end
         end
     end
@@ -261,38 +257,35 @@ end
 
 
 begin
-    #scripting to visualize blowups 
-    f = 0.4 
-    k = 0.35
-    St = Sts[2]
-    mass = 0.5
-    U_init = 1.0
+    #scripting
     upm = deepcopy(defaultDict)
     upm[:Nt] = 64
     upm[:N] = 64
     upm[:Ncycles] = 2
     upm[:Uinf] = 1.0
-    upm[:kine] = :make_ang
+    upm[:kine] = :make_heave_pitch
     upm[:pivot] = 0.0
     upm[:foil_type] = :make_naca
     upm[:thick] = 0.12
-    upm[:f] = f
-    upm[:k] = k
-    a0 = St / upm[:f]
-    upm[:motion_parameters] = [a0]
+    upm[:f] = 1.0
+    h0 = 0.0
+    θ0 = deg2rad(4)
+    upm[:motion_parameters] = [h0, θ0]
 
     foil, flow = init_params(; upm...)
-    
-    turns = zeros(flow.Ncycles * flow.N)
+    # foil.θ= π/2*0.0
+    U = _propel(foil, flow)
     wake = Wake(foil)
+
     Us = zeros(2, flow.Ncycles * flow.N)
-    U = _propel(foil, flow; U = [0.0, 0.0], mass = mass, turnto = turns[1])
-    #data containers
+
     old_mus, old_phis = zeros(3, foil.N), zeros(3, foil.N)
     phi = zeros(foil.N)
     coeffs = zeros(4, flow.Ncycles * flow.N)
     ps = zeros(foil.N, flow.Ncycles * flow.N)
-
+    turnrad = -π * flow.Δt / 4.0
+    trs = zeros(flow.N * flow.Ncycles)
+    trs[(flow.N):(2 * flow.N - 32)] .= turnrad
     anim = Animation()
     # f = plot()
     for i in 1:(flow.Ncycles * flow.N)
@@ -301,24 +294,23 @@ begin
             release_vortex!(wake, foil)
         end
         if i == 1
-            U = _propel(foil, flow; U = [0.0, 0.0], mass = mass)
+            U = (foil)(flow)
             # Cast this to  2d vel
-            U = [U_init, 0.0]
+            U = [0.0, 0.0]
         else
             U = _propel(foil,
                 flow;
                 forces = coeffs[:, i - 1],
-                U = U,
-                mass = mass,
-                self_prop = true,)        
+                U = [0.0, 0.0],
+                mass = 1e-5,
+                turn = trs[i],)
+            # U = (foil)(flow)
         end
-       
         Us[:, i] = U
-        U_bx = U[1]
         A, rhs, edge_body = make_infs(foil)
         A[getindex.(A .== diag(A))] .= 0.5
         setσ!(foil, flow)
-        
+        turn_σ!(foil, flow, trs[i])
         foil.wake_ind_vel = vortex_to_target(wake.xy, foil.col, wake.Γ, flow)
         normal_wake_ind = sum(foil.wake_ind_vel .* foil.normals, dims = 1)'
         foil.σs -= normal_wake_ind[:]
@@ -340,10 +332,10 @@ begin
         plot!(f, ylims = (-1, 1))
         plot!(title = "$(U)")
         frame(anim, f)
-        
+        # plot!(f, foil.foil[1,:],foil.foil[2,:], label="", aspect_ratio=:equal)
     end
-
-    gif(anim, "./images/blowups.gif", fps = 10)
+    # f
+    gif(anim, "./images/handp.gif", fps = 10)
 end
 
 begin
@@ -430,3 +422,57 @@ begin
     # f
     gif(anim, "./images/handp.gif", fps = 10)
 end
+
+begin
+    #running a heave and pitch sim
+    upm = deepcopy(defaultDict)
+    upm[:Nt] = 64
+    upm[:N] = 64
+    upm[:Ncycles] = 3
+    upm[:Uinf] = 1.0
+    upm[:kine] = :make_heave_pitch
+    upm[:pivot] = 0.0
+    upm[:foil_type] = :make_naca
+    upm[:thick] = 0.12
+    upm[:f] = 1.0
+    h0 = 0.0
+    θ0 = deg2rad(5)
+    upm[:motion_parameters] = [h0, θ0]
+
+    foil, flow = init_params(; upm...)
+    # grab the θ signal and then push it directly as a turning radius signal
+    # ensure that the σ values and performance metrics are the same for both methods
+    theta(_t) = foil.kine[2](foil.f, _t, -π / 2)
+    t = 1:(flow.Ncycles * flow.N)
+    t *= flow.Δt
+    plot(t, theta.(t))
+    flow.n = 0
+    #look at velocity decomposition first
+    turns = theta.(t) .* flow.Δt
+    discrete_vels = zeros(2, foil.N, flow.N * flow.Ncycles)
+    ad_vels = zeros(2, foil.N, flow.N * flow.Ncycles)
+    col = get_mdpts(foil._foil)
+    movie = @animate for (i, turn) in enumerate(turns)
+        # discrete_vels[:,:,i]= (rot(turn)*col-foil.col)
+        orig_pos = deepcopy(foil.col)
+        _propel(foil, flow; turnto = turn)
+        discrete_vels[:, :, i] = -(foil.col - orig_pos) ./ flow.Δt
+
+        v = get_panel_vels(foil, flow)
+        ad_vels[1, :, i] = v[1]
+        ad_vels[2, :, i] = v[2]
+        f = plot(foil.col[1, :], foil.col[2, :], aspect_ratio = :equal, ylims = (-0.3, 0.3))
+        f
+    end
+    gif(movie, "./images/forced_theta.gif")
+    offset = 5
+    au = plot(ad_vels[1, :, offset:end], st = :contourf)
+    av = plot(ad_vels[2, :, offset:end], st = :contourf)
+    du = plot(discrete_vels[1, :, offset:end] .- flow.Uinf, st = :contourf)
+    dv = plot(discrete_vels[2, :, offset:end], st = :contourf)
+    plot(au, av, du, dv, layout = (2, 2), size = (1200, 800))
+end
+
+a = plot(Us[1, :])
+b = plot(Us[2, :])
+plot(a, b, layout = (2, 1))
---
+++ src/Fence.jl
@@ -0,0 +1,247 @@
+using Dierckx
+
+
+function make_splines(foil, N)
+    top = Spline1D(foil[1,N÷2 + 1:end], foil[2,N÷2+1:end])   
+    bottom = Spline1D(foil[1,N÷2+1:-1:1], foil[2,N÷2+1:-1:1])
+    top,bottom 
+end
+
+function make_splines(foil)
+    # assumes traveling to the left
+    _ , N  = findmin(foil[1,:])
+    top    = Spline1D(foil[1,N:end],  foil[2,N:end])   
+    bottom = Spline1D(foil[1,N:-1:1], foil[2,N:-1:1])
+    top,bottom 
+end
+
+make_splines(foil::Foil) = make_splines(foil.foil)
+
+
+function foilsdf(foil::Foil)
+    #make splines
+    top, bottom = make_splines(foil)
+    """
+    isinside(xy)
+    xy is a pair to test if inside of the splines constructed
+    should allow for passing of vectors via 
+    
+    """
+    function isinside(xy)
+        #make a range of acceptable xs        
+        if minimum(foil.foil[1,:]) <= xy[1] <= maximum(foil.foil[1,:])
+            #find the smallest distance in y if accX is true
+            thetop = top(xy[1])
+            thebot = bottom(xy[1])
+            mid = (thetop + thebot)./2.0
+            if xy[2] <= mid
+                return thebot  - xy[2] 
+            else
+                return xy[2] .- thetop
+            end
+        else 
+            return 0.0
+        end
+    end
+end
+
+function foilsdf(foil::Foil, splines)
+    #make splines
+    top, bottom = splines
+    """
+    isinside(xy)
+    xy is a pair to test if inside of the splines constructed
+    should allow for passing of vectors via 
+    
+    """
+    function isinside(xy)
+        #make a range of acceptable xs        
+        if minimum(foil.foil[1,:]) <= xy[1] <= maximum(foil.foil[1,:])
+            #find the smallest distance in y if accX is true
+            thetop = top(xy[1])
+            thebot = bottom(xy[1])
+            mid = (thetop + thebot)./2.0
+            if xy[2] <= mid
+                return thebot  - xy[2] 
+            else
+                return xy[2] .- thetop
+            end
+        else 
+            return 0.0
+        end
+    end
+end
+
+function foilsdf(nfoil, N, splines)
+    #make splines
+    top,bottom = splines
+    
+    """
+    isinside(xy)
+    xy is a pair to test if inside of the splines constructed
+    should allow for passing of vectors via 
+    
+    """
+    function isinside(xy)
+        #make a range of acceptable xs        
+        if minimum(nfoil[1,:]) <= xy[1] <= maximum(nfoil[1,:])
+            #find the smallest distance in y if accX is true
+            thetop = top(xy[1])
+            thebot = bottom(xy[1])
+            mid = (thetop + thebot)./2.0
+            if xy[2] <= mid
+                return thebot  - xy[2] 
+            else
+                return xy[2] .- thetop
+            end
+        else 
+            return 0.0
+        end
+    end
+end
+
+function foilsdf(nfoil, N)
+    #make splines
+    top,bottom =make_splines(nfoil,N)
+    
+    """
+    isinside(xy)
+    xy is a pair to test if inside of the splines constructed
+    should allow for passing of vectors via 
+    
+    """
+    function isinside(xy)
+        #make a range of acceptable xs        
+        if minimum(nfoil[1,:]) <= xy[1] <= maximum(nfoil[1,:])
+            #find the smallest distance in y if accX is true
+            thetop = top(xy[1])
+            thebot = bottom(xy[1])
+            mid = (thetop + thebot)./2.0
+            if xy[2] <= mid
+                return thebot  - xy[2] 
+            else
+                return xy[2] .- thetop
+            end
+        else 
+            return 0.0
+        end
+    end
+end
+
+minsmax(foil) = map(x -> minimum(nfoil[1,:]) <= x <= maximum(nfoil[2,:]), dest[1,:])
+
+
+#TODO: refactor this into smaller functions for testing
+"""
+    sdf_fence(wake::Wake, foil::Foil, flow::FlowParams; dest = nothing)
+
+    dest -> the destination of the vortices
+    foil -> the foil to fence
+    flow -> the flow parameters
+    dest -> the destination of the vortices
+"""
+function sdf_fence(wake::Wake, foil::Foil, flow::FlowParams; dest = nothing, max_iters = 10)
+    # Estimate the final position of the vortices
+    dest = isnothing(dest) ? wake.xy + wake.uv * flow.Δt : dest    
+    # Calculate the next position of the foil
+    nfoil = next_foil_pos(foil, flow)
+    _ , N  = findmin(nfoil[1,:])
+    # Generate splines for the top and bottom paths of the foil
+    top, bottom = make_splines(nfoil)
+    # Construct signed distance functions (SDFs) for the foil
+    sdf = foilsdf(nfoil, foil.N, (top, bottom))
+    # Define the mid plane between splines
+    mid(x) = (top(x) + bottom(x)) / 2.0
+    
+    # Define vortex motion as a line with slopes and intercepts
+    ms = (dest[2, :] .- wake.xy[2, :]) ./ (dest[1, :] .- wake.xy[1, :])
+    bs = ms .* dest[1, :] .- dest[2, :]
+    # Check if the final position is inside the foil
+    xinside = map(x -> minimum(nfoil[1,:]) <= x <= maximum(nfoil[1,:]), dest[1,:])    
+    inside = map(x-> sdf(dest[:,x]) < 0, axes(dest,2) )
+    inside = inside .&& xinside
+    # Start the looping process, using a quadtree to reduce the computational load
+    iters = 1
+
+    while sum(inside) > 0 && iters < max_iters        
+        for i in findall(x -> x == 1, inside)
+            flip = 1  # Variable to flip the direction of the tangent vortex
+            # Check if the vortex is on the top or bottom of the foil
+            if dest[2, i] >= mid(wake.xy[1, i])
+                # Construct the spline path for the top of the foil
+                tPath = Spline1D(nfoil[1, N+1:end], nfoil[2, N+1:end] - ms[i] .* nfoil[1, N+1:end] .+ bs[i])
+                if wake.xy[1, i] < dest[1, i]
+                    xint = filter(x -> wake.xy[1, i] <= x <= dest[1, i], roots(tPath))
+                else
+                    xint = filter(x -> wake.xy[1, i] >= x >= dest[1, i], roots(tPath))
+                    flip = -1
+                end
+                # Check if there is an intersection point between the vortex and the foil
+                if !isempty(xint)
+                    yint = top(xint)
+                    whichPanel = findlast(xint .>= nfoil[1, N+1:end]) + N
+                else
+                    whichPanel = findlast(dest[1, i] .>= nfoil[1, N:end]) + N
+                end
+            else
+                # Construct the spline path for the bottom of the foil
+                bPath = Spline1D(nfoil[1, N:-1:1], nfoil[2, N:-1:1] - ms[i] .* nfoil[1,  N:-1:1] .+ bs[i])
+                if wake.xy[1, i] < dest[1, i]
+                    xint = filter(x -> wake.xy[1, i] <= x <= dest[1, i], roots(bPath))                    
+                else
+                    xint = filter(x -> wake.xy[1, i] >= x >= dest[1, i], roots(bPath))
+                    flip = -1
+                end
+                yint, whichPanel = process_intersection(xint, top, dest, i, nfoil, N)
+            end
+
+            dest[:, i] = update_dest(xint, dest, i, nfoil, whichPanel, foil, flow, yint, flip)
+            
+            xinside = map(x -> minimum(nfoil[1,:]) <= x <= maximum(nfoil[1,:]), dest[1,:])    
+            inside = map(x-> sdf(dest[:,x]) < 0, axes(dest,2) )
+            inside = inside .&& xinside
+            
+            iters += 1
+       
+        end
+    end
+    @assert count(x-> x ==2, inside) == 0 
+    dest    
+end
+
+function process_intersection(xint, path, dest, i, nfoil, N)
+    if !isempty(xint)
+        yint = path(xint)
+        whichPanel = findlast(xint .>= nfoil[1, N+1:end]) + N
+    else
+        whichPanel = findlast(dest[1, i] .>= nfoil[1, N:end]) + N
+    end
+    return yint, whichPanel
+end
+
+function update_dest(xint, dest, i, nfoil, whichPanel, foil, flow, yint, flip)
+    tangents = [-foil.tangents[:,1:foil.N÷2] foil.tangents[:,foil.N÷2+1:end]]
+    if !isempty(xint)
+        leg1 = [xint..., yint...] .- wake.xy[:, i] .+ flow.δ .* foil.normals[:, whichPanel]
+        leg2 = dest[:, i] .- [xint..., yint...] .+ flow.δ .* foil.normals[:, whichPanel]
+        mag = norm(leg2) * flip
+        final_pos = [xint..., yint...] + mag .* tangents[:, whichPanel] .+ flow.δ .* foil.normals[:, whichPanel]
+    elseif sdf(dest[:, i]) < 0.0
+        final_pos = dest[:, i] + nfoil[:, whichPanel] - foil.foil[:, whichPanel] .+ flow.δ .* foil.normals[:, whichPanel]
+    end
+    return final_pos
+end
+"""
+    sdf_fence(wake::Wake, foils::Vector,flow::FlowParams;dest=nothing, mask=nothing)
+
+    mask -> bit mask defaults to all foils get a fence, can select which get fences
+"""
+function sdf_fence(wake::Wake, foils::Vector, flow::FlowParams; mask=nothing) 
+    dest =  wake.xy + wake.uv * flow.Δt    
+    mask = isnothing(mask) ? ones(Bool, length(foils)) : mask
+    for i in findall(mask)
+
+        dest = sdf_fence(wake, foils[i], flow; dest=dest)              
+    end
+    dest
+end
