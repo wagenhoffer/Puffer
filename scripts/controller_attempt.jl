14f1e2216740640436d947da3f6fbff56fda0eb2
Wagenhoffer <wagenhoffer@psu.edu>
2024-05-15 11:43:02 -0400

    added controller to the perf net

--- scripts/controller_attempt.jl
+++ scripts/controller_attempt.jl
@@ -18,7 +18,7 @@ these = low .< (data[:,:reduced_freq] ./ data[:,:k] ) .< high
 count = sum(these)
 
 
-
+N = 64
 inputs = zeros(Float32, N , 2, 4, count)
 
 i = 1
@@ -97,24 +97,26 @@ plot(inputs[stride:stride:end,1,1,i], label="panels")
 plot!(foil.col[1,:], marker=:circle,lw=0, label="sample")
 
 
-dl = DataLoader((approx = images, RHS = RHS), batchsize=2048, shuffle=true)
+dl = DataLoader((approx = images, RHS = RHS, μs = latentdata.data.μs,
+                νs = latentdata.data.νs, βs = latentdata.data.βs,
+                perfs = latentdata.data.perfs ), batchsize=1024*4, shuffle=true)
 sd = dl|>first
 W,H,C,N = size(images)
-model = Chain(Conv((2,2), C=>C, Flux.tanh, pad=SamePad()),
-              Conv((4,2), C=>C, Flux.tanh, pad=SamePad()),
-              Conv((num_samps,1), C=>1, Flux.tanh, pad=SamePad()),
-              Flux.flatten,
-              Dense(W*H, W*H, Flux.tanh),              
-              Dense(W*H, 64)) |> gpu
-              
+model = Chain(Conv((4,2), C=>C, Flux.tanh, pad=SamePad()),                            
+              Flux.flatten,             
+              Dense(W*H*C, 64)) |> gpu
+model = Chain(Flux.flatten,             
+              Dense(W*H*C, W*H*C, Flux.tanh),
+              Dense(W*H*C, 64, Flux.tanh),
+              Dense(64, 64, Flux.tanh),) |> gpu              
 
 model(sd.approx|>gpu)
 
 state = Flux.setup(ADAM(0.01), model)
 
 losses = []
-for epoch = 1:100
-    for (x, y) in dl  
+for epoch = 1:500
+    for (x, y, _, _, _, _) in dl  
         x = x |> mp.dev
         y = y |> mp.dev
         ls = 0.0
@@ -181,3 +183,84 @@ val, this = findmin(encer)
 this = rand(1:size(images,4))
 plot(RHS[:,this], label="RHS")
 plot!(eout[:,this], seriestype=:scatter, label="model")
+
+
+pNN = Chain(Conv((4,2), 5=>5, Flux.tanh, pad = SamePad()),            
+            Conv((2,2), 5=>1,  pad = SamePad()),
+            Flux.flatten,   
+            Dense(16,mp.layers[end],Flux.tanh),
+            Dense(mp.layers[end],mp.layers[end],Flux.tanh),
+            Dense(mp.layers[end],3))|>mp.dev  
+pNN(images[:,:,:,1:4]|>mp.dev)
+pNNstate = Flux.setup(ADAM(0.01), pNN)
+# plosses = []
+
+# latim = permutedims(cat(latentdata.data.νs, latentdata.data.βs, dims=3), (1,3,2));
+# latim = reshape(latim, (size(latim)[1:2]...,1, size(latim,3)));
+# images = cat(images, latim, dims=3);
+# pndata = DataLoader((images = images, perfs = perfs, μs = μs ), batchsize=1024*4, shuffle=true)
+
+for epoch = 1:1000
+    for (ims, perf, μs) in pndata
+        ims = ims |> mp.dev        
+        lift = perf[2,:] |> mp.dev
+        thrust = perf[3,:] |> mp.dev
+        Γs = μs |> mp.dev
+        Γs = Γs[1,:] - Γs[end,:]
+        ls = 0.0
+        grads = Flux.gradient(pNN) do m
+            # Evaluate model and loss inside gradient context:
+            y = m(ims)
+            # ls  = errorL2(y[1,:], lift) #lift
+            ls = errorL2(y[2,:], thrust)#thrust  
+            ls += errorL2(y[3,:], Γs)             
+            
+        end
+        Flux.update!(pNNstate, pNN, grads[1])
+        push!(plosses, ls)  # logging, outside gradient context
+    end
+    if epoch %  mp.epochs /10 == 0
+        println("Epoch: $epoch, Loss: $(plosses[end])")
+    end
+end
+plosses
+ 
+begin
+
+    which = rand(0:199)
+    ns = 501
+   
+    lift = perfs[2,:] 
+    thrust = perfs[3,:] 
+    Γs = μs 
+    Γs = Γs[1,:] - Γs[end,:]
+        
+    y = pNN(pndata.data.images|>gpu)|>cpu
+            # Evaluate model and loss inside gradient context:
+            
+    lifte   = errorL2(y[1,:], lift) #lift
+    thruste = errorL2(y[2,:], thrust)#thrust  
+    gammae  = errorL2(y[3,:], Γs)             
+    @show (lifte, thruste, gammae)
+    le = []
+    te = []
+    ge = []
+    for i = 1:200
+        slice = ns*(i-1)+1:ns*i
+        push!(le, errorL2(y[1,slice], lift[slice]))
+        push!(te, errorL2(y[2,slice], thrust[slice]))
+        push!(ge, errorL2(y[3,slice], Γs[slice])   )
+    end
+
+    slice = ns*which+1:ns*(which+1)
+    @show coeffs[which, [:wave, :k,:reduced_freq]]
+    plot(lift[slice], label="lift")
+    a = plot!(y[1,slice], label="model")
+    plot(thrust[slice], label="thrust")
+    b = plot!(y[2,slice], label="model")
+    plot(Γs[slice], label="Γ")
+    c = plot!(y[3,slice], label="model")
+    plot(a,b,c, layout = (3,1), size = (1200,800))
+    # title!("k: $k, r: $r")
+
+end
